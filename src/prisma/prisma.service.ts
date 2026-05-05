262326c21280a85ab93227408fdcc8b9d240f4d8
Mohammad Kevin <kvn4.200581@gmail.com>
2026-05-05 08:26:34 +0700

    rack and box finish

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,8 +2,8 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {
-  user: any;
-  rack: any;
-    box: any;
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
 }
